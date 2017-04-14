1080f80be5b9b6bf9d6efd9d0a598c49bb8b4c23
Erik Dubois <erik.dubois@gmail.com>
2017-04-14 14:21:22 +0200

    updated cleanup.sh /g typo

--- archiso/airootfs/usr/bin/cleanup.sh
+++ archiso/airootfs/usr/bin/cleanup.sh
@@ -4,6 +4,6 @@ rm -R /etc/systemd/system/getty@tty1.service.d
 rm /etc/systemd/system/default.target
 rm /usr/share/applications/calamares.desktop
 rm -rf /abif-master
-sed -i 's/exec openbox-session/#exec openbox-session/' ~/.xinitrc
-sed -i 's/#exec openbox-session/exec openbox-session/' ~/.xsession
+sed -i 's/exec openbox-session/#exec openbox-session/g' ~/.xinitrc
+sed -i 's/#exec openbox-session/exec openbox-session/g' ~/.xsession
 sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' ~/.config/tint2/tint2rc
