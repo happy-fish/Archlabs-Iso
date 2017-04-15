a4aa3ed3b0a9d08e36661f105252cc7022d53843
Erik Dubois <erik.dubois@gmail.com>
2017-04-15 06:42:27 +0200

    moving to 3.4.1

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -14,6 +14,10 @@ useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storag
 #chmod 700 /root
 chown -R liveuser:users /home/liveuser
 
+#login lightdm
+groupadd -r autologin
+gpasswd -a liveuser autologin
+
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
@@ -23,11 +27,7 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-#solving obmenu crash
-#sed -i 's/exec openbox-session/#exec openbox-session/' ~/.xinitrc
-#sed -i 's/#exec openbox-session/exec openbox-session/' ~/.xsession
-
-systemctl enable pacman-init.service choose-mirror.service NetworkManager.service
-systemctl set-default multi-user.target
+systemctl enable pacman-init.service choose-mirror.service NetworkManager.service lightdm.service
+systemctl set-default graphical.target
 pacman -Syy
 gpg --receive-keys C1A60EACE707FDA5
