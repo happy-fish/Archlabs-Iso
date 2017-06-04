b83065ac9bce0982870f3f7d0e3f55267daa70bc
Erik Dubois <erik.dubois@gmail.com>
2017-06-04 14:48:40 +0200

    installation script

---
+++ how-to/install-applications-to-build-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+#======================================================================================
+#                                
+# Author  : Erik Dubois at http://www.erikdubois.be
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
+#======================================================================================
+
+echo "################################################################"
+echo "####             installing archiso                         ####"
+echo "################################################################"
+
+
+# installing refector to test wich servers are fastest
+sudo pacman -S archiso
+
+
+echo "################################################################"
+echo "####             Archiso installed                          ####"
+echo "################################################################"
+
