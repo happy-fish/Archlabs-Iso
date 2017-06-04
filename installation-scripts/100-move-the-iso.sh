a4ceaea25be28f4d7ffbb821f979a9674753372b
Erik Dubois <erik.dubois@gmail.com>
2017-06-04 17:58:02 +0200

    extra script to share to others

---
+++ installation-scripts/100-move-the-iso.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Archlabs 	: 	https://archlabsblog.wordpress.com/
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "moving iso to documents and insync" 
+
+
+cp ~/archlabs-build/archiso/out/archlabs-* ~/Documents/
+cp ~/archlabs-build/archiso/out/archlabs-* ~/Insync/Shared/ArchLabs/ISO/
+ 
