b84fdb23b92595572ab218cb6316986c7655a331
Erik Dubois <erik.dubois@gmail.com>
2017-06-24 17:30:55 +0200

    added password

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,8 +3,8 @@
 set -e -u
 
 iso_name=archlabs
-iso_label="archlabs-v4.1.13-$(date +%Y%m)-x86_64"
-iso_version="v4.1.13-$(date +%Y.%m.%d)"
+iso_label="archlabs-v4.1.14-$(date +%Y%m)-x86_64"
+iso_version="v4.1.14-$(date +%Y.%m.%d)"
 install_dir=arch
 work_dir=work
 out_dir=out
